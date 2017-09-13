000ef3e419f0b90eb5909be2e381a7f2eec474a2
tphoney <tp@puppet.com>
2017-09-13 14:11:46 +0100

    make tests work on redhat/centos

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper_acceptance'
 
 describe 'facter_task task' do
   describe 'puppet facts' do
-    it 'get a puppet fact' do
+    it 'get a puppet fact', if: fact('osfamily') == 'RedHat' do
       result = run_puppet_task(task_name: 'facter_task', params: { 'fact' => 'osfamily' })
       expect_multiple_regexes(result: result, regexes: [%r{status : RedHat}, %r{Job completed. 1/1 nodes succeeded}])
       result = run_puppet_task(task_name: 'facter_task', params: { 'fact' => 'operatingsystem' })
-      expect_multiple_regexes(result: result, regexes: [%r{status : CentOS}, %r{Job completed. 1/1 nodes succeeded}])
+      expect_multiple_regexes(result: result, regexes: [%r{status : (CentOS|RedHat)}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
 end
